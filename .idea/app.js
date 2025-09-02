ca7f32d0e986acddca00fe56dae8cb3dea22cf77
iremozrtr <iremozr16@gmail.com>
2025-09-02 15:45:36 +0300

    Prime number check

--- .idea/app.js
+++ .idea/app.js
@@ -1,46 +1,132 @@
-const number1Input = document.getElementById('number1');
-const number2Input = document.getElementById('number2');
-const resultInput = document.getElementById('result');
-const operatorButtons = document.querySelectorAll('.operator .btn-num');
+    const number1Input = document.getElementById('number1');
+    const number2Input = document.getElementById('number2');
+    const resultInput = document.getElementById('result');
+    const asalText = document.querySelector('.asal-text');
+
+    const addButton = document.querySelector('.operator button:nth-child(1)');
+    const subtractButton = document.querySelector('.operator button:nth-child(2)');
+    const multiplyButton = document.querySelector('.operator button:nth-child(3)');
+    const divideButton = document.querySelector('.operator button:nth-child(4)');
 
 
-const addButton = document.querySelector('.operator button:nth-child(1)');
-const subtractButton = document.querySelector('.operator button:nth-child(2)');
-const multiplyButton = document.querySelector('.operator button:nth-child(3)');
-const divideButton = document.querySelector('.operator button:nth-child(4)');
-document.addEventListener('DOMContentLoaded', () => {
-
-
-    function Addition(){
-        const num1=parseFloat(number1Input.value);
-        const num2=parseFloat(number2Input.value);
-        const result=num1+num2;
-        resultInput.value=result;
+    function changeResult(resultNumber) {
+        resultInput.value = resultNumber;
     }
-    addButton.addEventListener("click",Addition);
 
-    function Subtract(){
-        const num1=parseFloat(number1Input.value);
-       let num2=parseFloat(number2Input.value);
-        let result=num1-num2;
-        resultInput.value=result;
-
+    function isPrime(number) {
+        if (number <= 1 || !Number.isInteger(number)) {
+            return false;
+        }
+        if (number === 2) {
+            return true;
+        }
+        if (number % 2 === 0) {
+            return false;
+        }
+        for (let i=2;  i < number; i ++) {
+            if (number % i === 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    subtractButton.addEventListener("click", Subtract);
 
-    function Multiply(){
-        const num1=parseFloat(number1Input.value);
-        const num2=parseFloat(number2Input.value);
-        let result=num1*num2;
-        resultInput.value=result;
-    }
-    multiplyButton.addEventListener("click" ,Multiply);
 
-    function Divide(){
-        const num1=parseFloat(number1Input.value);
-        const num2=parseFloat(number2Input.value);
-        let result=num1/num2;
-        resultInput.value=result;
+    function Addition(num1,num2) {
+        return num1+num2;
     }
-   divideButton.addEventListener("click" ,Divide);
-});
+    addButton.addEventListener("click",()=>{
+        const num1 = parseFloat(number1Input.value) ;
+        const num2 = parseFloat(number2Input.value) ;
+        const addNumber = num1 + num2;
+        changeResult(addNumber);
+    if (!isNaN(addNumber)) {
+        if (addNumber % 1 === 0) {
+            const isResultPrime = isPrime(addNumber);
+            if (isResultPrime) {
+                asalText.textContent='is a prime number.';
+            } else {
+                asalText.textContent='is not a prime number. ';
+            }
+        } else {
+            asalText.textContent='prime number check is not working.';
+        }
+    }
+
+    });
+
+
+    function Subtract(num1,num2) {
+        return num1-num2;
+    }
+    subtractButton.addEventListener("click" , () => {
+        const num1 = parseFloat(number1Input.value) ;
+        const num2 = parseFloat(number2Input.value) ;
+        const substractNumber = num1 - num2;
+        changeResult(substractNumber);
+    if (!isNaN(substractNumber)) {
+        if (substractNumber % 1 === 0) {
+            const isResultPrime = isPrime(substractNumber);
+            if (isResultPrime) {
+                asalText.textContent='is a prime number.';
+            } else {
+                asalText.textContent='is not a prime number. ';
+            }
+        } else {
+            asalText.textContent='prime number check is not working.';
+        }
+    }
+
+
+    });
+
+
+    function Multiply(num1,num2) {
+        return num1*num2;
+    }
+    multiplyButton.addEventListener("click", ()=>{
+
+
+
+        const num1 = parseFloat(number1Input.value) ;
+        const num2 = parseFloat(number2Input.value) ;
+        const multiplyNumber = num1 * num2;
+        changeResult(multiplyNumber);
+        if (!isNaN(multiplyNumber)) {
+            if (isResultPrime) {
+                asalText.textContent='is a prime number.';
+            } else {
+                asalText.textContent='is not a prime number. ';
+            }
+        } else {
+            asalText.textContent='prime number check is not working.';
+        }
+
+
+
+    });
+
+    function Divide(num1, num2) {
+        return num1 / num2;
+    }
+    divideButton.addEventListener("click", () => {
+        const num1 = parseFloat(number1Input.value) ;
+        const num2 = parseFloat(number2Input.value) ;
+        const dividedNumber = Divide(num1, num2);
+        changeResult(dividedNumber);
+
+        if (!isNaN(dividedNumber)) {
+            if (dividedNumber % 1 === 0) {
+                const isResultPrime = isPrime(dividedNumber);
+                if (isResultPrime) {
+                    asalText.textContent='is a prime number.';
+                } else {
+                    asalText.textContent='is not a prime number. ';
+                }
+            } else {
+                asalText.textContent='prime number check is not working.';
+            }
+        }
+
+    });
+
